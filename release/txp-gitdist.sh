41a15760e69ac7dca488def808672976ba465d5e
Stef Dawson <thatbloke@stefdawson.com>
2018-03-08 11:19:54 +0000

    Bye bye .tar.gz. Symlinks in zip now

--- release/txp-gitdist.sh
+++ release/txp-gitdist.sh
@@ -63,10 +63,7 @@ rm textpattern-$VER/README.md
 rm -rf textpattern-$VER/.github
 
 # Bundle up.
-tar cvf - -C $DESTDIR textpattern-$VER | gzip -c > textpattern-$VER.tar.gz
-shasum -a 256 textpattern-$VER.tar.gz > textpattern-$VER.tar.gz.SHA256SUM
-
-zip --symlinks -r textpattern-$VER.zip textpattern-$VER --exclude textpattern-$VER/sites/\*
+zip --symlinks -r textpattern-$VER.zip textpattern-$VER
 shasum -a 256 textpattern-$VER.zip > textpattern-$VER.zip.SHA256SUM
 
 cd $OLDDIR
