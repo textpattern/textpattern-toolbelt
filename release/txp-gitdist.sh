03df83b443185d3949ae6b09757c4f03900c0a08
Pete Cooper <petecooper@users.noreply.github.com>
2020-09-13 13:17:24 +0100

    Update txp-gitdist.sh
    first pass at addressing https://github.com/textpattern/textpattern-toolbelt/issues/17

--- release/txp-gitdist.sh
+++ release/txp-gitdist.sh
@@ -65,7 +65,9 @@ echo 'Testing textpattern-$VER.tar.gz integrity...'
 if gzip -t textpattern-$VER.tar.gz; then
     echo 'textpattern-$VER.tar.gz passed `gzip -t` integrity test. Calculating SHA256 checksum...' \
     && shasum -a 256 textpattern-$VER.tar.gz > textpattern-$VER.tar.gz.SHA256SUM \
-    && echo 'SHA256 for textpattern-$VER.tar.gz: '$(cat textpattern-$VER.zip.SHA256SUM)
+    && echo 'SHA256 for textpattern-$VER.tar.gz: '$(cat textpattern-$VER.tar.gz.SHA256SUM) \
+    && echo 'Checking textpattern-$VER.tar.gz against textpattern-$VER.tar.gz.SHA256SUM...' \
+    && shasum -a 256 -c <<< '$(cat textpattern-$VER.zip.SHA256SUM) textpattern-$VER.tar.gz'
 else 
     echo 'textpattern-$VER.tar.gz failed `gzip -t` integrity test.'
 fi
@@ -75,7 +77,9 @@ echo 'Testing textpattern-$VER.zip integrity...'
 if unzip -t textpattern-$VER.`ip; then
     echo 'textpattern-$VER.zip passed `unzip -t` integrity test. Calculating SHA256 checksum...' \
     && shasum -a 256 textpattern-$VER.zip > textpattern-$VER.zip.SHA256SUM \
-    && echo 'SHA256 for textpattern-$VER.zip: '$(cat textpattern-$VER.zip.SHA256SUM)
+    && echo 'SHA256 for textpattern-$VER.zip: '$(cat textpattern-$VER.zip.SHA256SUM) \
+    && echo 'Checking textpattern-$VER.zip against textpattern-$VER.zip.SHA256SUM...' \
+    && shasum -a 256 -c <<< '$(cat textpattern-$VER.zip.SHA256SUM) textpattern-$VER.zip'
 else 
     echo 'textpattern-$VER.tar.gz failed `unzip -t` integrity test.'
 fi
