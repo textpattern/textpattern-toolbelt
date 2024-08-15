39638ea722530d03e74b197e6b6a12c2aae1e837
Pete Cooper <pete@pragmatika.net>
2024-08-15 18:04:37 +0100

    tidy and rearrange * quiet mode for archiving * clarity of output (work in progress)

--- release/txp-gitdist.sh
+++ release/txp-gitdist.sh
@@ -74,30 +74,42 @@ rm textpattern-$VER/textpattern/vendors/phpmailer/phpmailer/VERSION
 rm textpattern-$VER/textpattern/vendors/phpmailer/phpmailer/composer.json
 
 # Bundle up.
-tar cvf - -C $DESTDIR textpattern-$VER | gzip -c9 > textpattern-$VER.tar.gz
-echo 'Testing textpattern-'$VER'.tar.gz integrity...'
+echo -e "\n"
+echo '== Building textpattern-'$VER'.tar.gz in '$DESTDIR'...'
+tar cf - -C $DESTDIR textpattern-$VER | gzip -c9 -q > textpattern-$VER.tar.gz
+echo -e "\n"
+echo '== Building textpattern-'$VER'.zip in '$DESTDIR'...'
+zip --symlinks -r -q -9 textpattern-$VER.zip textpattern-$VER --exclude textpattern-$VER/sites/\*
+
+# Tests and checksums for .tar.gz
+echo -e "\n"
+echo '== Testing textpattern-'$VER'.tar.gz integrity...'
 if gzip -t textpattern-$VER.tar.gz; then
-    echo 'textpattern-'$VER'.tar.gz passed `gzip -t` integrity test. Calculating SHA256 checksum...' \
+    echo ' - textpattern-'$VER'.tar.gz passed `gzip -t` integrity test.' \
+    && echo ' - Calculating textpattern-'$VER'.tar.gz checksum...' \
     && shasum -a 256 textpattern-$VER.tar.gz > textpattern-$VER.tar.gz.SHA256SUM \
-    && echo 'SHA256 for textpattern-'$VER'.tar.gz: '$(< textpattern-$VER.tar.gz.SHA256SUM) \
-    && echo 'Checking textpattern-'$VER'.tar.gz against textpattern-'$VER'.tar.gz.SHA256SUM...' \
+    && echo ' - SHA256 for textpattern-'$VER'.tar.gz: '$(< textpattern-$VER.tar.gz.SHA256SUM) \
+    && echo ' - Checking textpattern-'$VER'.tar.gz checksum...' \
     && shasum -a 256 -c textpattern-$VER.tar.gz.SHA256SUM
 else 
-    echo 'textpattern-$VER.tar.gz failed `gzip -t` integrity test.'
+    echo ' - textpattern-$VER.tar.gz failed `gzip -t` integrity test.'
 fi
 
-zip --symlinks -r -9 textpattern-$VER.zip textpattern-$VER --exclude textpattern-$VER/sites/\*
-echo 'Testing textpattern-'$VER'.zip integrity...'
-if unzip -t textpattern-$VER.zip; then
-    echo 'textpattern-'$VER'.zip passed `unzip -t` integrity test. Calculating SHA256 checksum...' \
+# Tests and checksums for .zip
+echo -e "\n"
+echo '== Testing textpattern-'$VER'.zip integrity...'
+if unzip -q -t textpattern-$VER.zip; then
+    echo ' - textpattern-'$VER'.zip passed `unzip -t` integrity test.' \
+    && echo ' - Calculating textpattern-'$VER'.zip checksum...' \
     && shasum -a 256 textpattern-$VER.zip > textpattern-$VER.zip.SHA256SUM \
-    && echo 'SHA256 for textpattern-'$VER'.zip: '$(< textpattern-$VER.zip.SHA256SUM) \
-    && echo 'Checking textpattern-'$VER'.zip against textpattern-'$VER'.zip.SHA256SUM...' \
+    && echo ' - SHA256 for textpattern-'$VER'.zip: '$(< textpattern-$VER.zip.SHA256SUM) \
+    && echo ' - Checking textpattern-'$VER'.zip checksum...' \
     && shasum -a 256 -c textpattern-$VER.zip.SHA256SUM
 else 
-    echo 'textpattern-$VER.zip failed `unzip -t` integrity test.'
+    echo ' - textpattern-$VER.zip failed `unzip -t` integrity test.'
 fi
 
 cd $OLDDIR
 
-echo Textpattern v$VER built in $DESTDIR
+echo -e "\n"
+echo '== Textpattern v$VER built in '$DESTDIR
